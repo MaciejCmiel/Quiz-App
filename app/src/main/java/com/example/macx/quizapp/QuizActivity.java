6549af793762d670bcb226620c3179d9e4d9f577
MaciejCmiel <maciej.cmiel.S@ee.pw.edu.pl>
2018-02-03 00:16:45 +0100

    Update the question base

--- app/src/main/java/com/example/macx/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/macx/quizapp/QuizActivity.java
@@ -16,21 +16,20 @@ public class QuizActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayList<Question> yesNo = new ArrayList<Question>();
         ArrayList<Question> dates = new ArrayList<Question>();
-        ArrayList<Question> numbers = new ArrayList<Question>();
         ArrayList<Question> People = new ArrayList<Question>();
 
 
-        dates.add(new Question("When ...?", "1854" ));
-        dates.add(new Question("When ...?", "1854" ));
-        dates.add(new Question("When ...?", "1854" ));
-        dates.add(new Question("When ...?", "1854" ));
+        yesNo.add(new Question("Stive Jobs is the founder of Microsoft", "no"));
+        yesNo.add(new Question("Albert Einstein failed every subject in school that wasn't math or physics.", "yes"));
+        yesNo.add(new Question("Bill Gates is the founder of Microsoft.", "yes"));
+        yesNo.add(new Question("Tea has more caffeine than soda and coffee", "no"));
 
-
-        numbers.add(new Question("How many is 12 to the power of 2?", "144" ));
-        numbers.add(new Question("How many is 13 to the power of 2?", "169" ));
-        numbers.add(new Question("How many is 14 to the power of 2?", "196" ));
-        numbers.add(new Question("How many is 15 to the power of 2?", "255" ));
+        dates.add(new Question("How many is 12 to the power of 2?", "144" ));
+        dates.add(new Question("How many is 13 to the power of 2?", "169" ));
+        dates.add(new Question("How many is 14 to the power of 2?", "196" ));
+        dates.add(new Question("How many is 15 to the power of 2?", "255" ));
 
         People.add(new Question("Who developed the theory of relativity?", "Albert Einstein" ));
         People.add(new Question("Who was leading Spartans in Thermopylae battle?", "Leonidas" ));
